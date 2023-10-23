46b3fd2023a5e6d3ab5e08133fb72b317d40070c
Maryautotester <118917126+Maryautotester@users.noreply.github.com>
2023-10-24 00:40:24 +0300

    add allureReport, jenkins conf file, remote Webdriver

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -6,7 +6,7 @@ timeout(30) {
             }
             stage('Run tests') {
                 tests_exit_code = sh(
-                        script: """"
+                        script: """
                 mvn test -Dbrowser=$BROWSER -Dbrowserversion=$BROWSER_VERSION -Dwebdriver.base.url=$BASE_URL -Dwebdriver.remote.url=$SELENOID_URL
                 """,
                         returnStatus: true
@@ -16,10 +16,10 @@ timeout(30) {
                 }
             }
             stage('Publish artifacts') {
-                sh "echo BASE_URL=$BASE_URL > target/allure-results/environment.properties"
-                sh "echo BROWSER=$BROWSER >> target/allure-results/environment.properties"
-                sh "echo BROWSER_VERSION=$BROWSER_VERSION >> target/allure-results/environment.properties"
-                sh "echo SELENOID_URL=$SELENOID_URL >> target/allure-results/environment.properties"
+                sh "echo BASE_URL=$BASE_URL > ./allure-results/environment.properties"
+                sh "echo BROWSER=$BROWSER >> ./allure-results/environment.properties"
+                sh "echo BROWSER_VERSION=$BROWSER_VERSION >> ./allure-results/environment.properties"
+                sh "echo SELENOID_URL=$SELENOID_URL >> ./allure-results/environment.properties"
                 allure([
                         includeProperties: false,
                         jdk: '',
